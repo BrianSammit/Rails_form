89e0a25b9f70db03026e8c77a711289d657bef03
alejandrotoledoweb <atoledofr@gmail.com>
2020-11-17 17:30:57 -0500

    add create method and params for user, create create.html page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
     def new
+        @user = User.new
     end
     def create
+        @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+        if @user.save
+            redirect_to new_user_path
+        else
+            render :new
+        end
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
     end
 end
