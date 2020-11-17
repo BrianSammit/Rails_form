cda0be59c2792fe13af40c3901222ec77881d889
Brian Sammit Cruz Rodriguez <briansammit.cruz@gmail.com>
2020-11-17 18:03:50 -0500

    Add form_tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
         @user = User.new
     end
     def create
-        @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        @user = User.new(user_params)
 
         if @user.save
             redirect_to new_user_path
