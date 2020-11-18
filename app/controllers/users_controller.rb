eba61b5877b1061a1ef5a57eeae40d7f2bbc415c
Brian Sammit Cruz Rodriguez <briansammit.cruz@gmail.com>
2020-11-18 11:12:51 -0500

    fix linters errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,30 @@
 class UsersController < ApplicationController
-    def new
-        @user = User.new
-    end
-    
-    def create
-        @user = User.new(user_params)
-         #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+  def new
+    @user = User.new
+  end
 
-        if @user.save
-            redirect_to new_user_path
-        else
-            render :new
-        end
+  def create
+    @user = User.new(user_params)
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to new_user_path
     end
 
-    def edit
-        @user = User.find(params[:id])
-    end 
-
-    def update
-        @user = User.find(params[:id])
-        @user.update(user_params)
-        redirect_to new_user_path
-      end
-    
-    def user_params
-        params.require(:user).permit(:username, :email, :password)
-    end
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
