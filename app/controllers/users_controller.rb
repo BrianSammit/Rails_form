c405122242e309d08df796332c6363e7c0d0d9d5
alejandrotoledoweb <atoledofr@gmail.com>
2020-11-18 10:11:30 -0500

    add form_for to User.new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,10 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+    
     def create
         @user = User.new(user_params)
+        # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
         if @user.save
             redirect_to new_user_path
